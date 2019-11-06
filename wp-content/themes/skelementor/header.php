01401aca73bb33038545ce69e54ed497d69143e7
Zvi <io.windygo.co.il>
2019-11-06 10:36:56 +0200

    first commit

---
+++ wp-content/themes/skelementor/header.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+
+<?php
+if ( ! function_exists( 'elementor_theme_do_location' ) || ! elementor_theme_do_location( 'header' ) ) {
+	?>
+	
+<header id="site-header" class="site-header" role="banner">
+
+
+	
+	
+
+	
+	
+	<!-- <nav>
+
+	</nav> -->
+
+</header>
+
+<?php
+}
