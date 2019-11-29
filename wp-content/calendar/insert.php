6acd8d0b592981ae9d245a1fa30148b59b76a95a
Zvi <io.windygo.co.il>
2019-11-29 23:14:57 +0200

    Insert Google Calendar Event

--- wp-content/calendar/insert.php
+++ wp-content/calendar/insert.php
@@ -1,15 +1,86 @@
 <?php
-// Insert New Event
-// Refer to the PHP quickstart on how to setup the environment:
-// https://developers.google.com/calendar/quickstart/php
-// Change the scope to Google_Service_Calendar::CALENDAR and delete any stored
-// credentials.
+require __DIR__ . '/vendor/autoload.php';
+
+if (php_sapi_name() != 'cli') {
+    throw new Exception('This application must be run on the command line.');
+}
+
+/**
+ * Returns an authorized API client.
+ * @return Google_Client the authorized client object
+ */
+function getClient()
+{
+    $client = new Google_Client();
+    $client->setApplicationName('Google Calendar API PHP Quickstart');
+    $client->setScopes(Google_Service_Calendar::CALENDAR_READONLY);
+    $client->setAuthConfig('credentials.json');
+    $client->setAccessType('offline');
+    $client->setPrompt('select_account consent');
+
+    // Load previously authorized token from a file, if it exists.
+    // The file token.json stores the user's access and refresh tokens, and is
+    // created automatically when the authorization flow completes for the first
+    // time.
+    $tokenPath = 'token.json';
+    if (file_exists($tokenPath)) {
+        $accessToken = json_decode(file_get_contents($tokenPath), true);
+        $client->setAccessToken($accessToken);
+    }
+
+    // If there is no previous token or it's expired.
+    if ($client->isAccessTokenExpired()) {
+        // Refresh the token if possible, else fetch a new one.
+        if ($client->getRefreshToken()) {
+            $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
+        } else {
+            // Request authorization from the user.
+            $authUrl = $client->createAuthUrl();
+            printf("Open the following link in your browser:\n%s\n", $authUrl);
+            print 'Enter verification code: ';
+            $authCode = trim(fgets(STDIN));
+
+            // Exchange authorization code for an access token.
+            $accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
+            $client->setAccessToken($accessToken);
+
+            // Check to see if there was an error.
+            if (array_key_exists('error', $accessToken)) {
+                throw new Exception(join(', ', $accessToken));
+            }
+        }
+        // Save the token to a file.
+        if (!file_exists(dirname($tokenPath))) {
+            mkdir(dirname($tokenPath), 0700, true);
+        }
+        file_put_contents($tokenPath, json_encode($client->getAccessToken()));
+    }
+    return $client;
+}
 
 
-curl --request POST \
-  'https://www.googleapis.com/calendar/v3/calendars/windygo.co.il_u4vgnsknqi36hvk8l1crbuhgo4%40group.calendar.google.com/events?maxAttendees=2&sendNotifications=true&sendUpdates=all&key=[YOUR_API_KEY]' \
-  --header 'Authorization: Bearer [YOUR_ACCESS_TOKEN]' \
-  --header 'Accept: application/json' \
-  --header 'Content-Type: application/json' \
-  --data '{"summary":"Surf Lesson - 2PAX - 1 hour","description":"WC ORDER # 99999, 054-8899887","end":{"dateTime":"2019-11-30T11:00:00+02:00"},"start":{"dateTime":"2019-11-30T10:00:00+02:00"}}' \
-  --compressed
+// Get the API client and construct the service object.
+$client = getClient();
+$service = new Google_Service_Calendar($client);
+
+// Print the next 10 events on the user's calendar.
+$calendarId = 'windygo.co.il_u4vgnsknqi36hvk8l1crbuhgo4@group.calendar.google.com';
+
+
+
+POST https://www.googleapis.com/calendar/v3/calendars/windygo.co.il_u4vgnsknqi36hvk8l1crbuhgo4%40group.calendar.google.com/events?maxAttendees=2&sendNotifications=true&sendUpdates=all&key=AIzaSyBcJ1HJNjFr0gakkwcn440COQdp_wAxsk4 HTTP/1.1
+
+Authorization: Bearer [YOUR_ACCESS_TOKEN]
+Accept: application/json
+Content-Type: application/json
+
+{
+  "summary": "Surf Lesson - 2PAX - 1 hour",
+  "description": "WC ORDER # 99999, 054-8899887",
+  "end": {
+    "dateTime": "2019-11-30T11:00:00+02:00"
+  },
+  "start": {
+    "dateTime": "2019-11-30T10:00:00+02:00"
+  }
+}
